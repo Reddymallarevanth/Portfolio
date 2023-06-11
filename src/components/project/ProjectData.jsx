875cf43cb65c662a4c3c093876edf3e16bce1738
Reddymallarevanth <71754843+Reddymallarevanth@users.noreply.github.com>
2023-06-11 09:44:29 -0500

    committing the code

---
+++ src/components/project/ProjectData.jsx
@@ -0,0 +1,30 @@
+import leetcodeNotifier from "../../assets/leetcode.svg";
+import portfolio from "../../assets/portfolio.svg";
+import ecommerce from "../../assets/ecommerce.svg";
+import medical from "../../assets/medical.svg"
+
+const projectData = [
+    {
+        title : "Mail Order Pharmacy",
+        description : "Mail Order Pharmacy is a web development application created using the java spring boot and microservices",
+        image : medical,
+        projectLink : "https://github.com/Reddymallarevanth/Mail-Order-Pharmacy",
+    },
+    {
+        title : "Portfolio Website",
+        description : "Portfolio website created using Reactjs. Where I showcased my skills,projects and contact.",
+        image : portfolio,
+        projectLink : "https://github.com/Reddymallarevanth/Portfolio",
+    },
+    {
+        title : "Books Auction",
+        description : "E-commerce API made using the Ruby on Rails and Javascript where people can go for auction on books.",
+        image : ecommerce,
+        projectLink : "https://github.com/Reddymallarevanth/Rubyonrails-project",
+    },
+  
+    
+
+];
+
+export default projectData;
